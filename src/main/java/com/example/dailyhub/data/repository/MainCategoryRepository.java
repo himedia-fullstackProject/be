1935b239a66ecbeefa0b67e4377610afbcb594ee
minhwan9230 <xxcc96@naver.com>
2024-12-27 17:14:59 +0900

    수정사항반영

--- src/main/java/com/example/dailyhub/data/repository/MainCategoryRepository.java
+++ src/main/java/com/example/dailyhub/data/repository/MainCategoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.dailyhub.data.repository;
 
+import com.example.dailyhub.data.entity.MainCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MainCategoryRepository extends JpaRepository<MainCategoryRepository, Long> {
+public interface MainCategoryRepository extends JpaRepository<MainCategory, Long> {
 }
