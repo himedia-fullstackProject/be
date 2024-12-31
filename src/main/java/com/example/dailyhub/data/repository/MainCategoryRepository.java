ed176db6ebe47997bed3fd7edcbafd601a6c5497
Zara8170 <wlsgusan123@gmail.com>
2024-12-31 11:50:08 +0900

    delete

--- src/main/java/com/example/dailyhub/data/repository/MainCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dailyhub.data.repository;
-
-import com.example.dailyhub.data.entity.MainCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MainCategoryRepository extends JpaRepository<MainCategory, Long> {
-}
--- src/main/java/com/example/dailyhub/data/repository/SubCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dailyhub.data.repository;
-
-import com.example.dailyhub.data.entity.SubCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
-}
